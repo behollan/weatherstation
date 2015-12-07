a95dedf3d8ba4a63e9841de3d6542f07a6f5467f
Ben Holland <benjamin.holland1@gmail.com>
2015-12-06 18:52:12 -0700

    Test
    Test

--- weatherstation.ino
+++ weatherstation.ino
@@ -40,9 +40,19 @@
 #include <Wire.h> //I2C library
 #include "SparkFunMPL3115A2.h"
 #include "HIH_6130.h"
+#include "SparkFunTSL2561.h"
 
 #define TRUE 1
 #define FALSE 0
+
+//Light!!
+SFE_TSL2561 light; // For the Luminosity sensor
+
+boolean gain;     // Gain setting, 0 = X1, 1 = X16;
+unsigned int ms;  // Integration ("shutter") time in milliseconds
+
+
+//End Luminosity sensor things
 
 //Create an instance of the object
 MPL3115A2 myPressure;
@@ -60,67 +70,107 @@ void setup(void)
    delay(1000);
    Serial.println(">>>>>>>>>>>>>>>>>>>>>>>>");  // just to be sure things are working
 
-//Barometer
-   myPressure.begin(); // Get sensor online
 
-  // Configure the sensor
-  myPressure.setModeBarometer(); // Measure pressure in Pascals from 20 to 110 kPa
+   light.begin();
+   gain = 0;
+   unsigned char timelight = 2;
+   Serial.println("Set timing...");
+  // light.setTiming(gain, timelight, ms);
 
-  myPressure.setOversampleRate(7); // Set Oversample to the recommended 128
-
-  myPressure.enableEventFlags(); // Enable all three pressure and temp event flags 
+   Serial.println("Powerup...");
+   light.setPowerUp();
 
 }
     
 void loop(void)
 {
+	Serial.print("1");
 //Humidity and Temperature HIH
-   byte _status;
-   unsigned int H_dat, T_dat;
-   float RH, temperatureHIH, tempavg;
+//   byte _status;
+//   unsigned int H_dat, T_dat;
+//   float RH, temperatureHIH, tempavg;
+//
+//   _status = fetch_humidity_temperature(&H_dat, &T_dat); //Fetch temp and humidity data, as well as chip status
+//      
+//       switch(_status) //Use the status code from the HIH-6130 to print the current state of the chip
+//      {
+//          case 0:  Serial.println("Normal.");
+//                   break;
+//          case 1:  Serial.println("Stale Data.");
+//                   break;
+//          case 2:  Serial.println("In command mode.");
+//                   break;
+//          default: Serial.println("Diagnostic."); 
+//                   break; 
+//      }       
+//	  Serial.println(H_dat);
+//	  Serial.println(T_dat);
+//      RH = (float) H_dat * 6.10e-3;  //Per data sheet instructions for conversion to percent RH
+//      temperatureHIH = (float) T_dat * 1.007e-2 - 40.0; //convert temperature data per data sheet instruction to degrees C
+//
+//
+////Barometer MPL
+////Barometer
+//	  //myPressure.begin(); // Get sensor online
+//
+//			//			  // Configure the sensor
+//	  //myPressure.setModeBarometer(); // Measure pressure in Pascals from 20 to 110 kPa
+//
+//	  //myPressure.setOversampleRate(7); // Set Oversample to the recommended 128
+//
+//	  //myPressure.enableEventFlags(); // Enable all three pressure and temp event flags 
+//
+//  float pressure = myPressure.readPressure();
+//  Serial.print("Pressure(kPa):");
+//  Serial.println(pressure/1000, 2);
+//
+//  float temperatureMPL = myPressure.readTemp();
+//  //Serial.print(" Temp(c):");
+//  //Serial.print(temperature, 2);
+//
+//  //float temperature = myPressure.readTempF();
+//  //Serial.print(" Temp(f):");
+//  //Serial.print(temperature, 2);
 
-   _status = fetch_humidity_temperature(&H_dat, &T_dat); //Fetch temp and humidity data, as well as chip status
-      
-      switch(_status) //Use the status code from the HIH-6130 to print the current state of the chip
-      {
-          case 0:  Serial.println("Normal.");
-                   break;
-          case 1:  Serial.println("Stale Data.");
-                   break;
-          case 2:  Serial.println("In command mode.");
-                   break;
-          default: Serial.println("Diagnostic."); 
-                   break; 
-      }       
-	  Serial.println(H_dat);
-	  Serial.println(T_dat);
-      RH = (float) H_dat * 6.10e-3;  //Per data sheet instructions for conversion to percent RH
-      temperatureHIH = (float) T_dat * 1.007e-2 - 40.0; //convert temperature data per data sheet instruction to degrees C
+
+  //Light Sensor
+
+  
+
+  unsigned int data0, data1;
+
+  if (light.getData(data0, data1))
+  {
+
+	  Serial.print("data0: ");
+	  Serial.print(data0);
+	  Serial.print(" data1: ");
+	  Serial.print(data1);
+	  double lux;
+	  boolean good;
+	  good = light.getLux(gain, ms, data0, data1, lux);
+	  Serial.print(" lux: ");
+	  Serial.print(lux);
+	  if (good) Serial.println(" (good)"); else Serial.println(" (BAD)");
+  }
+  else
+  {
+	  byte error = light.getError();
+	  printError(error);
+  }
 
 
-//Barometer MPL
-
-  float pressure = myPressure.readPressure();
-  Serial.print("Pressure(kPa):");
-  Serial.println(pressure/1000, 2);
-
-  float temperatureMPL = myPressure.readTemp();
-  //Serial.print(" Temp(c):");
-  //Serial.print(temperature, 2);
-
-  //float temperature = myPressure.readTempF();
-  //Serial.print(" Temp(f):");
-  //Serial.print(temperature, 2);
+  //END LIGHT
 
 
 //General
-      tempavg=(temperatureMPL+temperatureHIH)/2; //Average MPL and HIH temp data
+  //    tempavg=(temperatureMPL+temperatureHIH)/2; //Average MPL and HIH temp data
       
 	  Serial.print("Relative Humidity(%): ");
-	  print_float(RH, 1); //Print Relative Humidity Percentage
+	//  print_float(RH, 1); //Print Relative Humidity Percentage
 	  Serial.print("\n"); //Line break to accomodate print_float
 	  Serial.print("Temperature(C): ");
-	  Serial.println(tempavg,2);
+	//  Serial.println(tempavg,2);
       Serial.println();
 
 
@@ -135,4 +185,34 @@ void loop(void)
 }
 
 
+//LIGHT and GENERAL but I like The error report so I kept it
 
+void printError(byte error)
+// If there's an I2C error, this function will
+// print out an explanation.
+{
+	Serial.print("I2C error: ");
+	Serial.print(error, DEC);
+	Serial.print(", ");
+
+	switch (error)
+	{
+	case 0:
+		Serial.println("success");
+		break;
+	case 1:
+		Serial.println("data too long for transmit buffer");
+		break;
+	case 2:
+		Serial.println("received NACK on address (disconnected?)");
+		break;
+	case 3:
+		Serial.println("received NACK on data");
+		break;
+	case 4:
+		Serial.println("other error");
+		break;
+	default:
+		Serial.println("unknown error");
+	}
+}
